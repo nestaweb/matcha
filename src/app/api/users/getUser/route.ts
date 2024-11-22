b17776e22107dbd55b9e3f253173dcdfbb419401
nestaweb <nesta.web.dev@gmail.com>
2024-11-22 14:05:37 +0100

    set gender register

---
+++ src/app/api/users/getUser/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from 'next/server';
+import pool from '@/server/db';
+import { CryptoService } from '@/server/CryptoService';
+
+export async function POST(req: NextRequest) {
+	if (!req.body) {
+		return NextResponse.json({ error: 'Missing body' }, { status: 400 });
+	}
+	try {
+		const { encryptedUserId } = await req.json();
+
+		const cryptedUserId = encryptedUserId.split('.');
+		const cryptedKeyUserId = { encryptedText: cryptedUserId[0], iv: cryptedUserId[1] };
+
+		const cryptoService = new CryptoService(process.env.ENCRYPTION_KEY!);
+
+		const userId = parseInt(cryptoService.decrypt(cryptedKeyUserId));
+
+		if (!userId || userId === undefined) {
+			return NextResponse.json({ error: 'Missing User ID' }, { status: 400 });
+		}
+
+		const user = await pool.query(
+			'SELECT * FROM users WHERE id = $1',
+			[userId]
+		);
+
+		if (user.rows.length === 0) {
+			return NextResponse.json({ error: 'User does not exist' }, { status: 404 });
+		}
+
+		console.log('Query successful:', user.rows);
+		return NextResponse.json(user.rows[0].age, { status: 200 });
+  	}
+	catch (error: any) {
+		console.error('Database connection error:', error);
+		return NextResponse.json({ 
+			error: error.message, 
+			stack: error.stack 
+		}, { status: 500 });
+	}
+}
--- src/components/custom/Register/Steps.tsx
+++ src/components/custom/Register/Steps.tsx
@@ -20,12 +20,12 @@ import { z } from "zod";
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 const registerFormSchema = z.object({
 	firstName: z.string().min(2, {
@@ -241,6 +241,23 @@ export const SecondStep: React.FC<StepsProps> = ({  }) => {
 	const router = useRouter();
 
 	useEffect(() => {
+		if (encryptedUserId === null || encryptedUserId === ".") {
+			router.push('/register');
+		}
+
+		fetch(`/api/users/getUser`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({encryptedUserId: encryptedUserId}),
+		})
+		.then(async (response) => {
+			if (response.status !== 200) {
+				router.push('/register');
+			}
+		})
+
 		fetch(`/api/users/getUserStatus`, {
 			method: 'POST',
 			headers: {
@@ -334,7 +351,7 @@ export const SecondStep: React.FC<StepsProps> = ({  }) => {
 			<h2 className='font-medium text-3xl'>Verification code</h2>
 			<Form {...otpForm}>
 				<form onSubmit={otpForm.handleSubmit(onSubmit)} className="flex flex-col w-2/3 mt-[5vh] z-20 items-center gap-6">
-				<FormField
+					<FormField
 						control={otpForm.control}
 						name="otp"
 						render={({ field }) => (
@@ -367,12 +384,27 @@ export const SecondStep: React.FC<StepsProps> = ({  }) => {
 }
 
 export const ThirdStep: React.FC<StepsProps> = ({ }) => {
-	const [question, setQuestion] = useState(1);
 	const searchParams = useSearchParams();
+	const question = parseInt(searchParams.get('question') || '1');
 	const encryptedUserId = searchParams.get('userId');
 	const router = useRouter();
 
 	useEffect(() => {
+		fetch(`/api/users/getUser`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({encryptedUserId: encryptedUserId}),
+		})
+		.then(async (response) => {
+			if (response.status !== 200) {
+				router.push('/register');
+			}
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
 		fetch(`/api/users/getUserStatus`, {
 			method: 'POST',
 			headers: {
@@ -410,43 +442,91 @@ export const ThirdStep: React.FC<StepsProps> = ({ }) => {
 	)
 }
 
+const genderFormSchema = z.object({
+	gender: z.string()
+})
+
 export const QuestionOne: React.FC = () => {
+	const genderForm = useForm<z.infer<typeof genderFormSchema>>({
+		resolver: zodResolver(genderFormSchema),
+		defaultValues: {
+			gender: ""
+		},
+	});
+
+	const searchParams = useSearchParams();
+	const encryptedUserId = searchParams.get('userId');
+	const router = useRouter();
+
+	function onSubmit(values: z.infer<typeof genderFormSchema>) {
+		fetch(`/api/users/setUserGender`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({encryptedUserId: encryptedUserId, ...values}),
+		})
+		.then((response) => {
+			if (response.status === 200) {
+				router.push('/register?step=3&question=2&userId=' + encryptedUserId);
+			}
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+	}
+
 	return (
 		<>
 			<h2 className='text-xl font-medium text-foreground/80'>What&apos;s your gender ?</h2>
-			<div className='flex gap-6'>
-				<div className='flex flex-col gap-2 items-center'>
-					<div className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
-						{/* <div className='w-11 h-11 bg-foreground/80 rounded-full'></div> */}
-					</div>
-					<p className='text-foreground/60 text-center'>Not sure about it</p>
-				</div>
-				<div className='flex flex-col gap-2 items-center'>
-					<div className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
-						<div className='w-11 h-11 bg-foreground/80 rounded-full'></div>
-					</div>
-					<p className='text-foreground/60 text-center'>Male</p>
-				</div>
-				<div className='flex flex-col gap-2 items-center'>
-					<div className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
-						{/* <div className='w-11 h-11 bg-foreground/80 rounded-full'></div> */}
-					</div>
-					<p className='text-foreground/60 text-center'>Nonbinary</p>
-				</div>
-				<div className='flex flex-col gap-2 items-center'>
-					<div className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
-						{/* <div className='w-11 h-11 bg-foreground/80 rounded-full'></div> */}
-					</div>
-					<p className='text-foreground/60 text-center'>Female</p>
-				</div>
-				<div className='flex flex-col gap-2 items-center'>
-					<div className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
-						{/* <div className='w-11 h-11 bg-foreground/80 rounded-full'></div> */}
-					</div>
-					<p className='text-foreground/60 text-center'>Don&apos;t want to say</p>
-				</div>
-			</div>
-			<Button>Continue</Button>
+			<Form {...genderForm}>
+				<form onSubmit={genderForm.handleSubmit(onSubmit)} className="flex flex-col items-center gap-4">
+					<FormField
+						control={genderForm.control}
+						name="gender"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<RadioGroup.Root defaultValue="dontwanttosay" className='flex gap-6' onChange={(e) => field.onChange(e)} name={field.name}>
+										<div className="flex flex-col gap-2 items-center">
+											<RadioGroup.Item value="notsure" id="r1"  className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
+												<RadioGroup.Indicator className='w-11 h-11 bg-foreground/80 rounded-full'/>
+											</RadioGroup.Item>
+											<p className='text-foreground/60 text-center'>Not sure about it</p>
+										</div>
+										<div className="flex flex-col gap-2 items-center">
+											<RadioGroup.Item value="male" id="r2"  className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
+												<RadioGroup.Indicator className='w-11 h-11 bg-foreground/80 rounded-full'/>
+											</RadioGroup.Item>
+											<p className='text-foreground/60 text-center'>Male</p>
+										</div>
+										<div className="flex flex-col gap-2 items-center">
+											<RadioGroup.Item value="nonbinary" id="r3"  className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
+												<RadioGroup.Indicator className='w-11 h-11 bg-foreground/80 rounded-full'/>
+											</RadioGroup.Item>
+											<p className='text-foreground/60 text-center'>Non binary</p>
+										</div>
+										<div className="flex flex-col gap-2 items-center">
+											<RadioGroup.Item value="female" id="r4"  className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
+												<RadioGroup.Indicator className='w-11 h-11 bg-foreground/80 rounded-full'/>
+											</RadioGroup.Item>
+											<p className='text-foreground/60 text-center'>Female</p>
+										</div>
+										<div className="flex flex-col gap-2 items-center">
+											<RadioGroup.Item value="dontwanttosay" id="r5"  className='w-14 h-14 rounded-full border-2 border-foreground/15 flex items-center justify-center'>
+												<RadioGroup.Indicator className='w-11 h-11 bg-foreground/80 rounded-full'/>
+											</RadioGroup.Item>
+											<p className='text-foreground/60 text-center'>Don&apos;t want to say</p>
+										</div>
+									</RadioGroup.Root>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button>Continue</Button>
+				</form>
+			</Form>
 		</>
 	)
 }
